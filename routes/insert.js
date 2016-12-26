2b19987f929d874974b33922179e92ddb5b3bbb5
Andrew Hoang <ahoang18@bu.edu>
2016-12-26 18:43:43 -0500

    Standardized insert.js

--- routes/insert.js
+++ routes/insert.js
@@ -1,114 +1,77 @@
-//async
-var async = require('async');
+var async = require('async')
+var debug = require('debug')('insert')
+// Authentication
+var authenticate = require('./authenticate.js')
+var oauth2Client = authenticate.client
+var calendarAPI = authenticate.api.calendar('v3')
 
-//Authentication
-var authenticate = require('./authenticate.js');
-var oauth2Client = authenticate.client;
-var calendarAPI = authenticate.api.calendar('v3');
+var parser = require('./parse.js')
 
-//Parser
-var parser = require('./parse.js');
+const UNTIL_DATE = '20170503T120000Z'
 
-const UNTIL_DATE = '20170503T120000Z';
-
-function insertCalendar(req, res)
-{
+function insertCalendar (req, res) {
   calendarAPI.calendars.insert({
     auth: oauth2Client,
-    'resource' : {
-      'summary' : req.query.name
+    'resource': {
+      'summary': req.query.name
     }
-  }, function(err, calendarCreated) {
-      if (err) {
-        console.log('CREATE CALENDAR ERROR ' + err);
-        res.redirect('/error');
-      }
-      console.log('New calendar created - ' );
-      console.log('calendarId:' + calendarCreated.id);
-      insertClasses(req, res, calendarCreated);
-  });
+  }, function (err, calendarCreated) {
+    if (err) {
+      debug('CREATE CALENDAR ERROR ' + err)
+      return res.redirect('/error')
+    }
+    debug('New calendar created - ')
+    debug('calendarId:' + calendarCreated.id)
+    insertClasses(req, res, calendarCreated)
+  })
 }
 
-function insertClasses(req, res, calendarCreated)
-{
-  var classArray = parser.createClassArray(req);
+function insertClasses (req, res, calendarCreated) {
+  var classArray = parser.createClassArray(req)
   async.forEach(classArray, insertClass.bind(insertClass, calendarCreated),
-    function(err){
-      if(err) {
-        console.log('Error on class insertion ' + err);
-        res.redirect('/error');
+    function (err) {
+      if (err) {
+        debug('Error on class insertion ' + err)
+        return res.redirect('/error')
+      } else {
+        debug('Completed class insertion!')
+        return res.redirect('/clean?cid=' + calendarCreated.id)
       }
-      else {
-        console.log('Completed class insertion!');
-        res.redirect('/clean?cid=' + calendarCreated.id);
-      }
-    });
+    })
 }
 
-var insertClass = function (calendarCreated, arrayIndex, callback)
-{
-  var startDate = parser.getWeekDay(arrayIndex.d);
-  var startDateTime = parser.setDateTimeFormat(arrayIndex.tb, startDate);
-  var endDateTime = parser.setDateTimeFormat(arrayIndex.te, startDate);
+var insertClass = function (calendarCreated, arrayIndex, callback) {
+  var startDate = parser.getWeekDay(arrayIndex.d)
+  var startDateTime = parser.setDateTimeFormat(arrayIndex.tb, startDate)
+  var endDateTime = parser.setDateTimeFormat(arrayIndex.te, startDate)
 
   var event = {
-    "summary": arrayIndex.c,
-    "start": {
-      "dateTime": startDateTime,
-      "timeZone": "America/New_York"
+    'summary': arrayIndex.c,
+    'start': {
+      'dateTime': startDateTime,
+      'timeZone': 'America/New_York'
     },
-    "end": {
-      "dateTime": endDateTime,
-      "timeZone": "America/New_York"
+    'end': {
+      'dateTime': endDateTime,
+      'timeZone': 'America/New_York'
     },
-    "recurrence": [
-      "RRULE:FREQ=WEEKLY;UNTIL=" + UNTIL_DATE,//BU-SPECIFIC
+    'recurrence': [
+      'RRULE:FREQ=WEEKLYUNTIL=' + UNTIL_DATE
     ]
-  };
+  }
 
   calendarAPI.events.insert({
-      auth: oauth2Client,
-      calendarId: calendarCreated.id,
-      resource: event,
-    }, function(err, event)
-      {
-        if(err)
-        {
-          console.log("Error on insert");
-          return callback(err);
-        }
-        else {
-          return callback();
-        }
-      }
-  );
-}
-
-function insertEventErrorHandler(err, event, retryCount, callback)
-{
-  if(err) {
-    if(err.code == 403)
-    {
-      if(retryCount < 4)
-      {
-        console.log('Ratelimited, retrying...');
-        setTimeout(insertClass)
-      }
-      else
-      {
-        console.log('Too many ratelimit errors, exiting');
-        res.redirect('/error');
-      }
+    auth: oauth2Client,
+    calendarId: calendarCreated.id,
+    resource: event
+  }, function (err, event) {
+    if (err) {
+      debug('Error on insert')
+      return callback(err)
+    } else {
+      return callback()
     }
-    else
-    {
-      console.log('Unknown error on event insertion');
-      res.redirect('/error');
-    }
-  }
-  else {
-    callback();
-  }
+  })
 }
 
 module.exports = {
