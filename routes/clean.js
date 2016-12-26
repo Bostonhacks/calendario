08565927ccce4ac744021b46e0c503ef4ce8e3e3
Andrew Hoang <ahoang18@bu.edu>
2016-12-26 18:31:29 -0500

    Added debug statements and fixed failing callback

--- routes/clean.js
+++ routes/clean.js
@@ -1,180 +1,263 @@
 // Authentication
-var authenticate = require('./authenticate.js');
-var oauth2Client = authenticate.client;
-var calendarAPI = authenticate.api.calendar('v3');
-var newCalendarId;
+var authenticate = require('./authenticate.js')
+var oauth2Client = authenticate.client
+var calendarAPI = authenticate.api.calendar('v3')
+var newCalendarId = null
 
-//API globals
-var octTenStart = '2016-10-10T00:00:00-04:00';
-var octTenEnd = '2016-10-10T23:00:00-04:00';
-var octElevenStart = '2016-10-11T00:00:00-04:00';
-var octElevenEnd = '2016-10-11T23:00:00-04:00';
-var novTwentyThirdStart = '2016-11-23T00:00:00-04:00';
-var novTwentyFifthEnd = '2016-11-25T23:00:00-04:00';
+var debug = require('debug')('clean')
 
-var fieldsNeeded = 'items(description,id,summary, start, end)';
-var singleEventsBool = true;
-var orderByStart = 'startTime';
+const FEB_20_START = '2017-02-20T00:00:00-05:00'
+const FEB_20_END = '2017-02-20T23:00:00-05:00'
+const FEB_21_START = '2017-02-21T00:00:00-05:00'
+const FEB_21_END = '2017-02-21T23:00:00-05:00'
+const MAR_04_START = '2017-03-04T00:00:00-05:00'
+const MAR_12_END = '2017-03-12T23:00:00-05:00'
+const APR_17_START = '2017-04-17T00:00:00-05:00'
+const APR_17_END = '2017-04-17T23:00:00-05:00'
+const APR_19_START = '2017-04-19T00:00:00-05:00'
+const APR_19_END = '2017-04-19T23:00:00-05:00'
 
-var async = require('async');
+const fieldsNeeded = 'items(description,id,summary, start, end)'
+const singleEventsBool = true
+const orderByStart = 'startTime'
 
-function cleanUp(req, res)
-{
+var async = require('async')
+
+function cleanUp (req, res) {
   setCalId(req.query.cid,
-    function(){
-        async.waterfall([
-          deleteOctEleven,
-          moveOctTenToEleven,
-          deleteThanksgiving
+    function () {
+      async.waterfall([
+        DELETE_FEB_21,
+        MOVE_FEB_20_TO_21,
+        DELETE_SPR_BREAK,
+        DELETE_APR_19,
+        MOVE_APR_17_TO_19
       ], function (err, result) {
-          console.log(result);
-          res.redirect('/end');
+        debug(result)
+        res.redirect('/end')
       })
     }
-  );
-
+  )
 }
 
-function setCalId(calId, callback)
-{
-  newCalendarId = calId;
-  callback();
+function setCalId (calId, callback) {
+  newCalendarId = calId
+  callback()
 }
 
-function moveOctTenToEleven(callback)
-{
+function MOVE_FEB_20_TO_21 (callback) {
   calendarAPI.events.list({
     auth: oauth2Client,
     calendarId: newCalendarId,
     orderBy: orderByStart,
     singleEvents: singleEventsBool,
-    timeMax: octTenEnd,
-    timeMin: octTenStart,
+    timeMax: FEB_20_END,
+    timeMin: FEB_20_START,
     fields: fieldsNeeded
-  }, function(err, eventList){
+  }, function (err, eventList) {
     if (err) {
-      console.log('Error getting 10/10 ' + err);
-      callback(err);
-    }
-    else{
-        var items = eventList.items;
-        // console.log(eventList.items);
-        //this to fix time zone difference for late night classes
-        for(var i = 0, len = items.length; i < len; i++ ){
+      debug(err)
+      return callback(err)
+    } else {
+      var items = eventList.items
+      // this to fix time zone difference for late night classes
+      for (var i = 0, len = items.length; i < len; i++) {
+        var startPrefix = '2017-02-21'
+        var endPrefix = '2017-02-21'
+        if (items[i].start.dateTime.substring(11,13) == '00' || items[i].start.dateTime.substring(11,13) == '01' || items[i].start.dateTime.substring(11,13) == '02') {
+          startPrefix = '2017-02-22'
+        }
+        if (items[i].end.dateTime.substring(11,13) == '00' || items[i].end.dateTime.substring(11,13) == '01' || items[i].end.dateTime.substring(11,13) == '02') {
+          endPrefix = '2017-02-22'
+        }
 
-          var startPrefix = '2016-10-11';
-          var endPrefix = '2016-10-11';
-          if(items[i].start.dateTime.substring(11,13) == '00' || items[i].start.dateTime.substring(11,13) == '01' || items[i].start.dateTime.substring(11,13) == '02'){
-            startPrefix ='2016-10-12';
-          }
-          if(items[i].end.dateTime.substring(11,13) == '00' || items[i].end.dateTime.substring(11,13) == '01' || items[i].end.dateTime.substring(11,13) == '02'){
-            endPrefix ='2016-10-12';
-          }
+        var newStartDateTime = startPrefix + items[i].start.dateTime.substring(10)
+        var newEndDateTime = endPrefix + items[i].end.dateTime.substring(10)
 
-          var newStartDateTime = startPrefix + items[i].start.dateTime.substring(10);
-          var newEndDateTime = endPrefix + items[i].end.dateTime.substring(10);
-
-          var newDateTime = {
-            "end":
-              {
-                "dateTime": newEndDateTime,
-                "timeZone": 'America/New_York'
-              },
-            "start":
-              {
-                "dateTime": newStartDateTime,
-                "timeZone": 'America/New_York'
-              },
-              "summary": items[i].summary
+        var newDateTime = {
+          'end':
+            {
+              'dateTime': newEndDateTime,
+              'timeZone': 'America/New_York'
+            },
+          'start':
+            {
+              'dateTime': newStartDateTime,
+              'timeZone': 'America/New_York'
+            },
+          'summary': items[i].summary
+        }
+        calendarAPI.events.update({
+          auth: oauth2Client,
+          calendarId: newCalendarId,
+          eventId: items[i].id,
+          resource: newDateTime
+        }, function (err) {
+          if (err) {
+            debug('Error updating 02/20->02/21 ' + err)
+            return callback(err)
           }
-          calendarAPI.events.update({
-            auth: oauth2Client,
-            calendarId: newCalendarId,
-            eventId: items[i].id,
-            resource: newDateTime
-          }, function(err){
-            if (err) {
-              console.log('Error updating 10/10->10/11 ' + err);
-              callback(err);
-            }
-        });
+        })
       }
-      callback(null);
+      return callback(null)
     }
-  });
+  })
 }
 
-function deleteOctEleven(callback)
-{
+function DELETE_FEB_21 (callback) {
   calendarAPI.events.list({
     auth: oauth2Client,
     calendarId: newCalendarId,
     orderBy: orderByStart,
     singleEvents: singleEventsBool,
-    timeMax: octElevenEnd,
-    timeMin: octElevenStart,
+    timeMax: FEB_21_END,
+    timeMin: FEB_21_START,
     fields: fieldsNeeded
-  }, function(err, eventList){
+  }, function (err, eventList) {
     if (err) {
-      console.log('Error getting 10/11 ' + err);
-      callback(err);
-    }
-    else {
-        var items = eventList.items;
-        for(var i = 0, len = items.length; i < len; i++ ){
-          calendarAPI.events.delete({
-            auth: oauth2Client,
-            calendarId: newCalendarId,
-            eventId: items[i].id
-          }, function(err){
-            if (err) {
-              console.log('Error deleting 10/11 ' + err);
-              callback(err);
-            }
-            else {
-              callback(null);
-            }
-        });
+      debug(err)
+      return callback(err)
+    } else {
+      var items = eventList.items
+      for (var i = 0, len = items.length; i < len; i++) {
+        calendarAPI.events.delete({
+          auth: oauth2Client,
+          calendarId: newCalendarId,
+          eventId: items[i].id
+        }, function (err) {
+          if (err) {
+            debug('Error deleting ' + err)
+            return callback(err)
+          }
+          return callback(null)
+        })
       }
     }
-  });
+  })
 }
 
-//11/23-27
-function deleteThanksgiving(callback)
-{
+function DELETE_SPR_BREAK (callback) {
   calendarAPI.events.list({
     auth: oauth2Client,
     calendarId: newCalendarId,
     orderBy: orderByStart,
     singleEvents: singleEventsBool,
-    timeMax: novTwentyFifthEnd,
-    timeMin: novTwentyThirdStart,
+    timeMax: MAR_12_END,
+    timeMin: MAR_04_START,
     fields: fieldsNeeded
-  }, function(err, eventList){
+  }, function (err, eventList) {
     if (err) {
-      console.log('Error getting thanksgiving ' + err);
-      callback(err);
-    }
-    else {
-        var items = eventList.items;
-        for(var i = 0, len = items.length; i < len; i++ ){
-          calendarAPI.events.delete({
-            auth: oauth2Client,
-            calendarId: newCalendarId,
-            eventId: items[i].id
-          }, function(err){
-            if (err) {
-              console.log('Error deleting thanksgiving ' + err);
-              callback(err);
-            }
-            else {
-              callback(null, 'done deleting');
-            }
-        });
+      debug('Error getting spr_break ' + err)
+      callback(err)
+    } else {
+      var items = eventList.items
+      for (var i = 0, len = items.length; i < len; i++) {
+        calendarAPI.events.delete({
+          auth: oauth2Client,
+          calendarId: newCalendarId,
+          eventId: items[i].id
+        }, function (err) {
+          if (err) {
+            debug('Error deleting spr_break ' + err)
+            return callback(err)
+          } else {
+            return callback(null)
+          }
+        })
       }
     }
-  });
+  })
+}
+
+function MOVE_APR_17_TO_19 (callback) {
+  calendarAPI.events.list({
+    auth: oauth2Client,
+    calendarId: newCalendarId,
+    orderBy: orderByStart,
+    singleEvents: singleEventsBool,
+    timeMax: APR_17_END,
+    timeMin: APR_17_START,
+    fields: fieldsNeeded
+  }, function (err, eventList) {
+    if (err) {
+      debug(err)
+      return callback(err)
+    } else {
+      var items = eventList.items
+      // this to fix time zone difference for late night classes
+      for (var i = 0, len = items.length; i < len; i++) {
+        var startPrefix = '2017-04-19'
+        var endPrefix = '2017-04-19'
+        if (items[i].start.dateTime.substring(11,13) == '00' || items[i].start.dateTime.substring(11,13) == '01' || items[i].start.dateTime.substring(11,13) == '02') {
+          startPrefix = '2017-04-20'
+        }
+        if (items[i].end.dateTime.substring(11,13) == '00' || items[i].end.dateTime.substring(11,13) == '01' || items[i].end.dateTime.substring(11,13) == '02') {
+          endPrefix = '2017-04-20'
+        }
+
+        var newStartDateTime = startPrefix + items[i].start.dateTime.substring(10)
+        var newEndDateTime = endPrefix + items[i].end.dateTime.substring(10)
+
+        var newDateTime = {
+          'end': {
+            'dateTime': newEndDateTime,
+            'timeZone': 'America/New_York'
+          },
+          'start': {
+            'dateTime': newStartDateTime,
+            'timeZone': 'America/New_York'
+          },
+          'summary': items[i].summary
+        }
+
+        calendarAPI.events.update({
+          auth: oauth2Client,
+          calendarId: newCalendarId,
+          eventId: items[i].id,
+          resource: newDateTime
+        }, function (err) {
+          if (err) {
+            debug('Error updating 04/17->04/19 ' + err)
+            return callback(err)
+          }
+        })
+      }
+      return callback(null, 'Done cleaning dates!')
+    }
+  })
+}
+
+function DELETE_APR_19 (callback) {
+  calendarAPI.events.list({
+    auth: oauth2Client,
+    calendarId: newCalendarId,
+    orderBy: orderByStart,
+    singleEvents: singleEventsBool,
+    timeMax: APR_19_END,
+    timeMin: APR_19_START,
+    fields: fieldsNeeded
+  }, function (err, eventList) {
+    if (err) {
+      debug(err)
+      return callback(err)
+    } else {
+      var items = eventList.items
+      for (var i = 0, len = items.length; i < len; i++) {
+        calendarAPI.events.delete({
+          auth: oauth2Client,
+          calendarId: newCalendarId,
+          eventId: items[i].id
+        }, function (err) {
+          if (err) {
+            debug('Error deleting ' + err)
+            return callback(err)
+          } else {
+            return callback(null)
+          }
+        })
+      }
+    }
+  })
 }
 
 //date:'2016-02-16T23:00:00-05:00'
@@ -190,11 +273,11 @@ function deleteThanksgiving(callback)
 //     fields: 'items(description,id,summary, start, end)'
 //   }, function(err, eventList) {
 //     if (err) {
-//       console.log(err);
-//       return callback(err);
+//       debug(err)
+//       return callback(err)
 //     }
-//     return callback(eventList);
-//   });
+//     return callback(eventList)
+//   })
 // }
 //
 // function updateDay(newDateString, callback)
@@ -206,9 +289,9 @@ function deleteThanksgiving(callback)
 //     resource: newDateTime
 //   }, function(err){
 //     if (err) {
-//       callback(err);
+//       callback(err)
 //     }
-//   });
+//   })
 // }
 
 module.exports = {
